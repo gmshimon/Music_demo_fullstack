ac1b43555b79cdfce9b1737e7492b087a96117da
gmshimon <simon.rosedale99@gmail.com>
2025-08-21 00:42:43 +0200

    initialized the backedn server

---
+++ Backend/index.js
@@ -0,0 +1,44 @@
+import mongoose from 'mongoose'
+import express from 'express'
+import http from 'http'
+import cors from 'cors'
+import bodyParser from 'body-parser'
+import dotenv from 'dotenv'
+
+const { query } = express
+dotenv.config()
+const port = process.env.PORT || 5000
+
+const app = express()
+app.use(
+  cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+  })
+)
+const server = http.createServer(app)
+
+app.use(express.json({ limit: '50mb' }))
+app.use(express.urlencoded({ limit: '50mb', extended: true }))
+app.use(bodyParser.json())
+
+const uri = 'mongodb://127.0.0.1:27017/cms_website'
+// const uri = process.env.MONGODB_URI
+
+mongoose.connect(uri).then(() => {
+  console.log('🛢️ Connected to MongoDB')
+})
+
+app.get('/', async (req, res) => {
+  res.send('API is running')
+})
+
+// Global error handler
+// app.use(globalErrorHandler)
+
+server.listen(port, () => {
+  console.log(`🚀 Server is running on port ${port}`)
+})
+
+export default app
